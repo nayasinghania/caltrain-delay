a359041586d4a405137147216269fc58a8347f8c
Naya Singhania <74111380+nayasinghania@users.noreply.github.com>
2026-08-10 14:12:23 -0700

    basic app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,17 +1,44 @@
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { useState } from "react"
 
 export function App() {
+  const [vehicleId, setVehicleId] = useState("")
+  const [stationName, setStationName] = useState("")
+  const [delay, setDelay] = useState(0)
+
+  function submit() {
+    fetch("http://localhost:8000/predict", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ vehicle_id: Number(vehicleId), station_name: stationName }),
+    })
+      .then((res) => res.json())
+      .then((data) => setDelay(data.result));
+  }
   return (
     <div className="flex min-h-svh p-6">
       <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
         <div>
-          <h1 className="font-medium">Caltrain Delay Predictor</h1>
-          <p>Predict train delay times within 1.5 minutes of actual arrival time</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
+          <h1>Caltrain Delay Predictor</h1>
+          <p>
+            Predict train delay times within 1.5 minutes of actual arrival time
+          </p>
+          <Input
+            placeholder="Enter train number"
+            value={vehicleId}
+            onChange={(e) => setVehicleId(e.target.value)}
+          />
+          <Input
+            placeholder="Enter station name"
+            value={stationName}
+            onChange={(e) => setStationName(e.target.value)}
+          />
+          <Button className="mt-2" onClick={submit}>Submit</Button>
+          <p className="mt-2">{delay != 0 ? delay.toFixed(2) + " minutes delayed" : "Enter the above information to get a prediction"}</p>
+          <div className="font-mono text-xs text-muted-foreground mt-2">
+            (Press <kbd>d</kbd> to toggle dark mode)
+          </div>
         </div>
       </div>
     </div>
